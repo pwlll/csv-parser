6e6ad5a8169acd9ff94242516ff6505c5bca2e28
pwlll <92825761+pwlll@users.noreply.github.com>
2024-10-02 00:58:18 +0200

    Added the sortList function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,15 +2,12 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         List<Participant> parList=new ArrayList<>();
         try(Scanner scanner=new Scanner(new FileInputStream("./src/main/java/5TR.csv"), StandardCharsets.UTF_16)){
-            System.out.println(scanner.hasNext());
             boolean flag=false;
             String tempLine;
             while(scanner.hasNextLine()){
@@ -19,7 +16,6 @@ public class Main {
                     flag=false;
                 }
                 if(flag && !tempLine.isBlank()) parList.add(getValues(tempLine));
-
                 if(tempLine.equals("2. Uczestnicy")){
                     flag=true;
                     scanner.nextLine();
@@ -31,6 +27,15 @@ public class Main {
             System.out.println("File not found");
         }
         saveToJson(parList);
+
+        for(Participant par:parList){
+            par.showParticipant();
+        }
+        sortList(parList);
+        System.out.println("\n");
+        for(Participant par:parList){
+            par.showParticipant();
+        }
     }
     private static Participant getValues(String line){
         Participant participant=new Participant();
@@ -46,7 +51,6 @@ public class Main {
                     scanner.next()
             );
         }
-        participant.showParticipant();
         return participant;
     }
     private static void saveToJson(List<Participant> list){
@@ -56,5 +60,57 @@ public class Main {
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static List<Participant> sortList(List<Participant> list){
+        List<Integer> tempTime;
+        Scanner scn;
+        int totalTime;
+        for(int i=0;i<list.size();i++){
+            tempTime=new ArrayList<>();
+            scn=new Scanner(list.get(i).parTime);
+            scn.useDelimiter(" ");
+            totalTime=0;
+            while(scn.hasNext()){
+                try{
+                    tempTime.add(Integer.parseInt(scn.next()));
+                }
+                catch (NumberFormatException exception){}
+            }
+            switch(tempTime.size()){
+                case 1:
+                    totalTime+=tempTime.get(0);
+                    break;
+                case 2:
+                    totalTime+=tempTime.get(1);
+                    totalTime+=tempTime.get(0)*60;
+                    break;
+                case 3:
+                    totalTime+=tempTime.get(2);
+                    totalTime+=tempTime.get(1)*60;
+                    totalTime+=tempTime.get(0)*3600;
+                    break;
+            }
+            list.get(i).parTime=String.valueOf(totalTime);
+        }
+        Collections.sort(list,new Comparator(){
+            @Override
+            public int compare(Object o1, Object o2) {
+                Participant par1=(Participant) o1;
+                Participant par2=(Participant) o2;
+                return Integer.parseInt(par1.parTime)-Integer.parseInt(par2.parTime);
+            }
+        });
+        int hours,minutes,seconds;
+        String str;
+        for(int i=0;i<list.size();i++){
+            totalTime=Integer.parseInt(list.get(i).parTime);
+            hours = totalTime/ 3600;
+            minutes = (totalTime% 3600) / 60;
+            seconds = totalTime% 60;
+            str= String.format("%d godz. %d min %d s", hours, minutes, seconds);
+            list.get(i).parTime=str;
+        }
+        return list;
     }
 }
